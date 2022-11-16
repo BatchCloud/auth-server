ec777e509f5b07058792bc7d3a9db430fc450d98
Sebastian Steininger <batchcloud@web.de>
2022-11-16 18:42:13 +0100

    update user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'username' => $data['name'],
+            'firstname' => 'firstname',
+            'lastname' => 'lastname',
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
